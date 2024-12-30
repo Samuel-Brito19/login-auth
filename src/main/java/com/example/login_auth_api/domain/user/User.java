5d09f438d1e6eba918fcc55db034bc3e19b3a309
Samuel-Brito19 <sambriton7@gmail.com>
2024-12-30 19:54:43 -0300

    feat: Configurating token

--- src/main/java/com/example/login_auth_api/domain/user/User.java
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -23,4 +23,8 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/com/example/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.example.login_auth_api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.login_auth_api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,18 @@ public class TokenService {
             throw new RuntimeException("Erro ao logar");
         }
     }
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm).withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
     private Instant generateExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
